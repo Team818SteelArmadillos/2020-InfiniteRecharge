c0584136530162c198dffb086d4b1b5a5a89fd37
msimons6303 <38868932+msimons6303@users.noreply.github.com>
2020-01-16 18:45:31 -0500

    Cleaning
    Cleaning up copyright comments and adding sample code in comments

--- src/main/java/frc/robot/auton/commandGroupAuton.java
+++ src/main/java/frc/robot/auton/commandGroupAuton.java
@@ -1,24 +1,25 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.auton;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+
 public class commandGroupAuton extends SequentialCommandGroup {
-  /**
-   * Creates a new commandGroupAuton.
-   */
+
   public commandGroupAuton() {
-    // Add your commands in the super() call, e.g.
-    // super(new FooCommand(), new BarCommand());
-    super();
+   // add sequentials for different positions
+   // if (autonPosition == 1){
+  // addSequential();
+  // addSequential();
+  // addSequential();
+  // } else if (autonPosition == 2){
+ // addSequential();
+  // addSequential();
+  // addSequential();
+ // } else if (autonPosition ==3){
+ // addSequential();
+  // addSequential();
+  // addSequential();
+ // }
+   
   }
 }
