9d40b3aeb20c4197048e47e2a40ef65927be814b
Emily Feuer <emilymfeuer@gmail.com>
2020-01-16 19:51:01 -0500

    Updated Vision Code
    Added image display when code is run

---
+++ python vision code/Vision code 2.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+img = cv2.imread("python vision code\\2020SampleVisionImages\\WPILib Robot Vision Images\\BlueGoal-060in-Center.jpg")
+VisionCode(img)
+cv2.imshow('res',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+def VisionCode(frame):
+    pass 
