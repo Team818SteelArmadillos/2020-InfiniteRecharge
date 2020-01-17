a6bfcc9872e19eb022a74da40230cb880de286eb
msimons6303 <38868932+msimons6303@users.noreply.github.com>
2020-01-16 19:39:14 -0500

    RobotLog and Psuedocode
    Added RobotLog as a class and to command and subsystem. Made psuedocode in commandgroup for auton.

--- src/main/java/frc/robot/auton/commandGroupAuton.java
+++ src/main/java/frc/robot/auton/commandGroupAuton.java
@@ -2,24 +2,25 @@ package frc.robot.auton;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-
 public class commandGroupAuton extends SequentialCommandGroup {
 
   public commandGroupAuton() {
-   // add sequentials for different positions
-   // if (autonPosition == 1){
-  // addSequential();
-  // addSequential();
-  // addSequential();
-  // } else if (autonPosition == 2){
- // addSequential();
-  // addSequential();
-  // addSequential();
- // } else if (autonPosition ==3){
- // addSequential();
-  // addSequential();
-  // addSequential();
- // }
-   
+    if (autonPosition == 1) {
+      addSequential();
+      addSequential();
+      addSequential();
+      RobotLog.putMessage("Running Auton Position 1");
+    } else if (autonPosition == 2) {
+      addSequential();
+      addSequential();
+      addSequential();
+      RobotLog.putMessage("Running Auton Position 2");
+    } else if (autonPosition == 3) {
+      addSequential();
+      addSequential();
+      addSequential();
+      RobotLog.putMessage("Running Auton Position 3");
+    }
+
   }
 }
