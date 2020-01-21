c9a3ca8b23bb7e7faf358f3a1654ae46474f93e4
Emily Feuer <emilymfeuer@gmail.com>
2020-01-21 17:38:31 -0500

    Fixed Vision code 2
    Image was not appearing when vision code was run- fixed

--- python vision code/Vision code 2.py
+++ python vision code/Vision code 2.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 img = cv2.imread("python vision code\\2020SampleVisionImages\\WPILib Robot Vision Images\\BlueGoal-060in-Center.jpg")
-VisionCode(img)
+VisionCode = (img)
 cv2.imshow('res',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
