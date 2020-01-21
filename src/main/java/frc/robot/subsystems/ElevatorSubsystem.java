ef2761542908acb310d1de1deca960ecac7affef
msimons6303 <38868932+msimons6303@users.noreply.github.com>
2020-01-21 18:23:16 -0500

    Added Subsystem
    Added psuedocode and code for elevator in subsystem

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,13 +1,36 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import frc.robot.RobotLog;
 
 public class ElevatorSubsystem extends SubsystemBase {
-  
-  public ElevatorSubsystem() {
+  static TalonSRX elevatorMotor;
+// change below later in season
+  static int tolerance = 1000;
+  static int ifOn = 0;
 
-  }
-  public void periodic() {
-    
+  public ElevatorSubsystem(int elevatorMotor) {
+    elevator = new DoubleSolenoid(elevatorMotor);
+// talonsrx motor 
+// 1 button 
+// double solenoid
+// anticpate 1 pnumatic actuator - ratcheting system
+
+  RobotLog.putMessage("Running ElevatorSubsystem");
+}
+
+public void setElevatorMotor(double Speed){
+  elevator.set(Speed);
+}
+
+  public void elevatorButton(int ifOn) {
+    if (buttonhit){
+      ifOn = ifOn + 1;
+    } else if (button active & the nhit){
+      ifOn = ifOn - 1;
+    } else {
+      // Nothing
+    }
   }
 }
