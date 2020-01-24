ce4a7a0763d0eb4407f91a92a1c8b8ee762533f4
msimons6303 <38868932+msimons6303@users.noreply.github.com>
2020-01-23 19:47:16 -0500

    Timers
    These include the timers so that the piston doesnt break when the motors start running. This DOES NOT INCLUDE the timer used when the robot is interuppted, shut off, or ends a match. We need to get the timer from the field of the match so we shut down motors and wait half a secto put piston in so we stay where we are no matter what.

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -3,12 +3,16 @@ package frc.robot.commands;
 import frc.robot.Robot;
 // import frc.robot.subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Timer;
 //import frc.robot.RobotLog;
 
 public class ElevatorCommand extends CommandBase {
+  Timer pistonTimer;
 
   public ElevatorCommand(int pistonVal) {
     addRequirements(Robot.m_elevatorSubsystem);
+
+    pistonTimer = new Timer();
   }
 
   public void initialize() {
@@ -18,15 +22,35 @@ public class ElevatorCommand extends CommandBase {
 
   public void execute() {
     if (Robot.m_oi.getElevatorUp()) {
-      Robot.m_elevatorSubsystem.setElevatorMotors(.7);
       Robot.m_elevatorSubsystem.setPiston(0.5);
+      pistonTimer.start();
+      if (pistonTimer.hasPeriodPassed(0.5)) {
+        Robot.m_elevatorSubsystem.setElevatorMotors(.7);
+        pistonTimer.stop();
+        pistonTimer.reset();
+      }
     } else if (Robot.m_oi.getElevatorDown()) {
-      Robot.m_elevatorSubsystem.setElevatorMotors(.7);
       Robot.m_elevatorSubsystem.setPiston(0.5);
+      pistonTimer.start();
+      if (pistonTimer.hasPeriodPassed(0.5)) {
+        Robot.m_elevatorSubsystem.setElevatorMotors(.7);
+        pistonTimer.stop();
+        pistonTimer.reset();
+      }
+
     } else {
-      Robot.m_elevatorSubsystem.setElevatorMotors(1);
+      Robot.m_elevatorSubsystem.setElevatorMotors(0);
+      pistonTimer.start();
+      if (pistonTimer.hasPeriodPassed(0.5))
+        Robot.m_elevatorSubsystem.setElevatorMotors(1);
+      pistonTimer.stop();
+      pistonTimer.reset();
     }
   }
+
+  // idk if it goes in finished or not but we need to get timer in feild so it
+  // shuts off motors, half a
+  // sec passes, and piston is put in to be safe
 
   public boolean isFinished() {
     return false;
