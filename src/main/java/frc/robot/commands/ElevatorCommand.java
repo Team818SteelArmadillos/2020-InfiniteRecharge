83ef4e3bfb70e81e1a5b9d7023e66c8fcf56a837
msimons6303 <38868932+msimons6303@users.noreply.github.com>
2020-01-24 19:28:47 -0500

    Added new timer
    Added timer from field so we auto stop when climbing if the match ends.

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -3,11 +3,13 @@ package frc.robot.commands;
 import frc.robot.Robot;
 // import frc.robot.subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 //import frc.robot.RobotLog;
 
 public class ElevatorCommand extends CommandBase {
   Timer pistonTimer;
+  DriverStation driverStation;
 
   public ElevatorCommand(int pistonVal) {
     addRequirements(Robot.m_elevatorSubsystem);
@@ -33,18 +35,27 @@ public class ElevatorCommand extends CommandBase {
       Robot.m_elevatorSubsystem.setPiston(0.5);
       pistonTimer.start();
       if (pistonTimer.hasPeriodPassed(0.5)) {
-        Robot.m_elevatorSubsystem.setElevatorMotors(.7);
+        Robot.m_elevatorSubsystem.setElevatorMotors(-.7);
         pistonTimer.stop();
         pistonTimer.reset();
       }
 
+    } else if (driverStation.getMatchTime() <= 1.5) {
+      Robot.m_elevatorSubsystem.setElevatorMotors(0);
+      pistonTimer.start();
+      if (pistonTimer.hasPeriodPassed(0.5)) {
+        Robot.m_elevatorSubsystem.setPiston(1);
+        pistonTimer.stop();
+        pistonTimer.reset();
+      }
+      
     } else {
       Robot.m_elevatorSubsystem.setElevatorMotors(0);
       pistonTimer.start();
       if (pistonTimer.hasPeriodPassed(0.5))
-        Robot.m_elevatorSubsystem.setElevatorMotors(1);
-      pistonTimer.stop();
-      pistonTimer.reset();
+        Robot.m_elevatorSubsystem.setPiston(1);
+        pistonTimer.stop();
+        pistonTimer.reset();
     }
   }
 
