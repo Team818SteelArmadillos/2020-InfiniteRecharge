fb2db31c2c35e183c7e762b6cb0606b426f66c5b
msimons6303 <38868932+msimons6303@users.noreply.github.com>
2020-02-07 17:59:54 -0500

    Auton
    Added auton descriptiuojns

--- src/main/java/frc/robot/auton/AutonCommandGroup.java
+++ src/main/java/frc/robot/auton/AutonCommandGroup.java
@@ -2,27 +2,37 @@ package frc.robot.auton;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotLog;
+import frc.robot.commands.ShooterCommand;
 import frc.robot.commands.driveDistance;
+import frc.robot.commands.turnAngle;
 
 public class AutonCommandGroup extends SequentialCommandGroup {
 
-  public AutonCommandGroup() {
+  public AutonCommandGroup(double autonPosition) {
     // Auton 1 = Do nothing
     // Auton 2 = 
     // Auton 3 = 
-    // Auton 4 = 
+    // Auton 4 = Start facing the trench run. Deploy intake and drive forward to get first two balls. Turn around and drive in front of the Power Port.
+    // Shoot all five balls.
 
-    if(autonPosition==1) {
-    
+    if(autonPosition == 1) {
     RobotLog.putMessage("Running Auton Position 1");
-  } else if(autonPosition==2) {
-    addSequential(driveDistance(3));
+
+  } else if(autonPosition == 2) {
+    new driveDistance(3);
     RobotLog.putMessage("Running Auton Position 2");
-  } else if(autonPosition==3) {
-    addSequential();
-    addSequential();
-    addSequential();
+
+  } else if(autonPosition == 3) {
+    new ShooterCommand();
+    new driveDistance(3);
     RobotLog.putMessage("Running Auton Position 3");
+
+  } else if (autonPosition == 4){
+    new driveDistance(3);
+    new turnAngle(180);
+    new driveDistance(3);
+    new ShooterCommand();
   }
 
-}}
+}
+}
