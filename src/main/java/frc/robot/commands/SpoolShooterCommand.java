ee7ca45b082683fe7fe4c9c47a7143b4d3017ad2
JosephChreiki <56415575+ToyotaTruenoae86@users.noreply.github.com>
2020-02-28 18:46:49 -0500

    Update  10.1

--- src/main/java/frc/robot/commands/SpoolShooterCommand.java
+++ src/main/java/frc/robot/commands/SpoolShooterCommand.java
@@ -1,34 +1,29 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-import frc.robot.Constants.ShooterConstants.*;
-import frc.robot.subsystems.ShooterSubsystem.*;
-import frc.robot.subsystems.VisionSubsystem.*;
+
 
 public class SpoolShooterCommand extends CommandBase {
-  
-  Timer timer;
 
   public SpoolShooterCommand() {
     addRequirements(Robot.m_shooterSubsystem);
-    addRequirements(Robot.m_visionSubsystem);
-    addRequirements(Robot.m_indexSubsystem);
-    timer = new Timer();
-  }
+    }
 
   @Override
   public void initialize() {
-    Robot.m_indexSubsystem.setIndexMotor(0);
-    Robot.m_shooterSubsystem.shooterSpeed(1600);
+    
+    Robot.m_shooterSubsystem.shooterSpeed(0);
+
   }
 
   @Override
   public void execute() {
-    
+    if(Robot.m_oi.getSpoolShootButton()){
     Robot.m_shooterSubsystem.shooterSpeed(1600);
-  
+    }else{
+    Robot.m_shooterSubsystem.shooterSpeed(0);
+    }
   }
 
   @Override
